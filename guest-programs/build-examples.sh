133aa26e472191f68e139bc4a7e931a018b5df84
Andreas Tzionis <andy0n7@gmail.com>
2023-12-23 23:36:08 +0200

    minor fix

--- guest-programs/build-examples.sh
+++ guest-programs/build-examples.sh
@@ -3,11 +3,14 @@
 set -euo pipefail
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
-source ../ci/jobs/detect-or-install-riscv-toolchain.sh
-
-if [ "${RV32E_TOOLCHAIN:-}" == "" ]; then
-    echo "ERROR: rv32e toolchain is missing; PolkaVM binaries can't be built!"
-    exit 1
+if [[ "$(rustup toolchain list)" =~ "riscv32em-nightly-2023-04-05-r0-x86_64-unknown-linux-gnu" ]]; then
+    export RV32E_TOOLCHAIN="riscv32em-nightly-2023-04-05-r0-x86_64-unknown-linux-gnu"
+else
+    curl -L --output /tmp/rust-riscv32em-nightly-2023-04-05-r0-x86_64-unknown-linux-gnu.tar.xz "https://github.com/koute/rustc-rv32e/releases/download/nightly-2023-04-05-r0/rust-riscv32em-nightly-2023-04-05-r0-x86_64-unknown-linux-gnu.tar.xz"
+    tar -C /tmp -xf /tmp/rust-riscv32em-nightly-2023-04-05-r0-x86_64-unknown-linux-gnu.tar.xz
+    mkdir -p ~/.rustup/toolchains
+    mv /tmp/rust-riscv32em-x86_64-unknown-linux-gnu/riscv32em-nightly-2023-04-05-r0-x86_64-unknown-linux-gnu ~/.rustup/toolchains/
+    export RV32E_TOOLCHAIN="riscv32em-nightly-2023-04-05-r0-x86_64-unknown-linux-gnu"
 fi
 
 function build_example () {
